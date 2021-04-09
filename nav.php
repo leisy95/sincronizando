6f777b866824c5e52f918d407363d1f489e5bf71
Leisy Yasmin Ladino <leisy95.ladino@gmail.com>
2021-04-09 15:45:39 -0500

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ol>
+        <li><a href="#"> home </a></li>
+        <li><a href="#"> Quienes </a></li>
+        <li><a href="#"> Nosotros </a></li>
+        <li><a href="#"> Contactos </a></li>
+    </ol>
+</nav>
