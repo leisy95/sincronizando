689dbb4a3993f4fc1ce743369aa1dfc4d3a416a5
Leisy Yasmin Ladino <leisy95.ladino@gmail.com>
2021-04-09 15:58:08 -0500

    merge

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <nav>
     <ol>
         <li><a href="#"> home </a></li>
@@ -7,13 +7,4 @@
         <li><a href="#"> Contactos </a></li>
     </ol>
 </nav>
-=======
-<header>
-  <ol>
-      <li><a href="#"> home </a></li>
-      <li><a href="#"> Quienes </a></li>
-      <li><a href="#"> Nosotros </a></li>
-      <li><a href="#"> Contactos </a></li>
-  </ol>
-</header>
->>>>>>> bd6679e12b6e76f7dc351ecfb13003b9ab123c63
+
