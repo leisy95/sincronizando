bd6679e12b6e76f7dc351ecfb13003b9ab123c63
Leisy Ladino <82046545+leisy95@users.noreply.github.com>
2021-04-09 15:37:04 -0500

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<header>
+  <ol>
+      <li><a href="#"> home </a></li>
+      <li><a href="#"> Quienes </a></li>
+      <li><a href="#"> Nosotros </a></li>
+      <li><a href="#"> Contactos </a></li>
+  </ol>
+</header>
